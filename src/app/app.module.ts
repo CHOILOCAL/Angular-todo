57998c902d7631dd06f6b2398d85049401250c77
최현지(q900ta) <q900ta@shinsegae.com>
2020-03-09 11:15:39 +0900

    create a section component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import {HelloComponent} from "./hello/hello.component";
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+
     HelloComponent,
-    HeaderComponent,
-    FooterComponent
+    HeaderComponent, // ng generate component header
+    FooterComponent, SectionComponent // ng g c footer, section
   ],
   imports: [
     BrowserModule
