9b57abccbe87363459c03355850b75485f74a194
최현지(q900ta) <q900ta@shinsegae.com>
2020-03-09 14:22:57 +0900

    section group

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {HelloComponent} from "./hello/hello.component";
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SectionComponent } from './section/section.component';
+import { ButtonsComponent } from './section/buttons/buttons.component';
+import { TimeDisplayComponent } from './section/time-display/time-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
 
-    HelloComponent,
     HeaderComponent, // ng generate component header
-    FooterComponent, SectionComponent // ng g c footer, section
+    FooterComponent, SectionComponent, ButtonsComponent, TimeDisplayComponent // ng g c footer, section
   ],
   imports: [
     BrowserModule
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'] // 명시된 모든 .css를 넣어준다.
 })
 export class HeaderComponent implements OnInit {
 
